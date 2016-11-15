3cdca71db9726609e9d3494d4a709377216b6df7
kaledragule <ben.diel@gmail.com>
2016-11-15 17:03:01 -0500

    more stuff

---
+++ queries/sessions/runningSQL.sql
@@ -0,0 +1,14 @@
+select x.sid
+      ,x.serial#
+      ,x.username
+      ,x.sql_id
+      ,x.sql_child_number
+      ,optimizer_mode
+      ,hash_value
+      ,address
+      ,sql_text
+from   v$sqlarea sqlarea
+      ,v$session x
+where  x.sql_hash_value = sqlarea.hash_value
+and    x.sql_address    = sqlarea.address
+and    x.username       is not null;
