2a17961918c08432ff963da7ee23c16508fc1d27
kaledragule <ben.diel@gmail.com>
2017-08-29 16:45:08 -0400

    System Files Resize

--- queries/tablespace/systemDatafileResize.sql
+++ queries/tablespace/systemDatafileResize.sql
@@ -1,5 +1,10 @@
-set lines 1000 pages 1000 head off
+set lines 300 pages 200 head off
 select 'alter database datafile '''
 || file_name 
 || ''' autoextend on maxsize 5G;'
-from dba_data_files where regexp_like(file_name, 'system|sysaux|undotbs1|users');
+from dba_data_files where regexp_like(file_name, 'system|sysaux|undotbs1|users')
+Union
+select 'alter database tempfile '''
+|| file_name 
+|| ''' autoextend on maxsize 10G;'
+from dba_temp_files where regexp_like(file_name, 'temp');
